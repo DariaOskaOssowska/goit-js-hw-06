b88f8ef69935de5e66f84bf11d30e4da3864dbf9
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-07-17 18:04:23 +0200

    Update task-03.js

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -12,3 +12,18 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const list = document.querySelector('.gallery')
+
+const markup = images
+  .map(image => `<li><img class="picture" src=${image.url} alt='${image.alt}'></img></li>`)
+  .join('');
+
+  list.insertAdjacentHTML('beforeend', markup)
+    // const img = document.createElement('img')
+  
+const pictures = document.querySelectorAll('.picture')
+
+list.style.display = 'flex';
+list.style.flexWrap = 'wrap';
+
