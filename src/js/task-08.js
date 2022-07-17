bf5a846143e9ee3c39eba5de55dd12d70721467c
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-07-17 20:20:07 +0200

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector('.login-form')
 
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!')
+    }
+    const userDetails = { email: email.value, Password: password.value }
+    
+    console.log(userDetails)
+    event.currentTarget.reset()
+}
