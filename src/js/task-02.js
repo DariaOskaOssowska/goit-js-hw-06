b666410500aa3562cc20febd244dfe197defd6ff
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-07-17 16:46:51 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,14 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
+
+const list = document.querySelector('#ingredients')
+const liArray = []
+
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li')
+	item.className = 'item'
+	item.textContent = ingredient
+	liArray.push(item)
+})
+
+list.append(...liArray)
+
