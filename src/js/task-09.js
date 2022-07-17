c05166e7f54b6e395c3a11e143f240a95f4bbfad
Daria Ośka-Ossowska <daria.oska@gmail.com>
2022-07-17 20:54:04 +0200

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButton = document.querySelector('button.change-color')
+const body = document.querySelector('body')
+const colorValue = document.querySelector('span.color')
+
+
+changeButton.addEventListener('click', e => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+})
+
